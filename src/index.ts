5ba86890f2e60089b7cc766978a31c5d657845f8
vonbarown <vonielbrown@pursuit.org>
2020-08-22 15:59:47 -0400

    mikroOrm setup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
