507e8bd046d580899adf1f69c9a6939d00826751
vonbarown <vonielbrown@pursuit.org>
2020-09-05 10:49:14 -0400

    pagination start in backend

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -12,6 +12,7 @@ import {
 import { Post } from "src/entities/Posts";
 import { MyContext } from "src/types";
 import { isAuth } from "src/middleware/isAuth";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -24,8 +25,22 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return Post.find();
+  async posts(
+    @Arg("limit") limit: number,
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit);
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder("post")
+      .orderBy('"createdAt"', "DESC")
+      .take(realLimit);
+
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+    }
+
+    return qb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
